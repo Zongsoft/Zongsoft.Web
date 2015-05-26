66b295fd62edd32e1fb681e67f96b03a044f9a12
Popeye Zhong <zongsoft@gmail.com>
2015-05-26 19:48:53 +0800

    添加了一个Button控件。

---
+++ src/Controls/Button.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2015 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web.
+ *
+ * Zongsoft.Web is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Web; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+
+namespace Zongsoft.Web.Controls
+{
+	public class Button : DataBoundControl
+	{
+		#region 构造函数
+		public Button()
+		{
+			this.CssClass = "btn button";
+		}
+		#endregion
+
+		#region 公共属性
+		[Bindable(true)]
+		public string Name
+		{
+			get
+			{
+				return this.GetPropertyValue(() => this.Name);
+			}
+			set
+			{
+				this.SetPropertyValue(() => this.Name, value);
+			}
+		}
+
+		[Bindable(true)]
+		public string Value
+		{
+			get
+			{
+				return this.GetPropertyValue(() => this.Value);
+			}
+			set
+			{
+				this.SetPropertyValue(() => this.Value, value);
+			}
+		}
+		#endregion
+
+		#region 重写方法
+		protected override void RenderBeginTag(HtmlTextWriter writer)
+		{
+			if(string.IsNullOrWhiteSpace(this.Name) && (!string.IsNullOrWhiteSpace(this.ID)))
+				writer.AddAttribute(HtmlTextWriterAttribute.Name, this.ID);
+
+			this.AddAttributes(writer);
+			writer.RenderBeginTag(HtmlTextWriterTag.Button);
+		}
+
+		protected override void RenderEndTag(HtmlTextWriter writer)
+		{
+			writer.RenderEndTag();
+		}
+		#endregion
+	}
+}
