31756ca81961da4754ec9ae48ba5922de288d63f
Popeye Zhong <zongsoft@gmail.com>
2015-07-24 23:12:02 +0800

    根据新的设计规范，重新调整授权验证的实现逻辑。 :monkey: :+1:

--- src/Mvc/AuthorizationFilter.cs
+++ src/Mvc/AuthorizationFilter.cs
@@ -61,56 +61,96 @@ namespace Zongsoft.Web.Mvc
 		#region 验证方法
 		public void OnAuthorization(AuthorizationContext filterContext)
 		{
-			var schemaId = string.Empty;
-			var actionId = string.Empty;
+			string schemaId, actionId;
 
-			var controllerAttribute = (AuthorizationAttribute)Attribute.GetCustomAttribute(filterContext.Controller.GetType(), typeof(Zongsoft.Security.Membership.AuthenticationAttribute), true);
+			//获取授权验证的方式
+			var mode = this.GetAuthorizationMode(filterContext, out schemaId, out actionId);
 
-			if(controllerAttribute == null || string.IsNullOrWhiteSpace(controllerAttribute.SchemaId))
-				schemaId = this.GetSchemaId(filterContext.Controller.GetType().Name);
-			else
-				schemaId = controllerAttribute.SchemaId;
-
-			var actionAttribute = (AuthorizationAttribute)filterContext.ActionDescriptor.GetCustomAttributes(typeof(Zongsoft.Security.Membership.AuthenticationAttribute), true).FirstOrDefault();
-
-			if(actionAttribute != null)
+			switch(mode)
 			{
-				if(!string.IsNullOrWhiteSpace(actionAttribute.SchemaId))
-					schemaId = actionAttribute.SchemaId;
+				case AuthorizationMode.Ignore: //忽略授权验证
+					return;
+				case AuthorizationMode.Licensee: //身份验证(即需要验证当前操作者是否为非匿名用户)
 
-				actionId = string.IsNullOrWhiteSpace(actionAttribute.ActionId) ? filterContext.ActionDescriptor.ActionName : actionAttribute.ActionId;
+					//如果当前操作为匿名操作（即操作者没有登录验证），则返回验证失败的响应
+					if(!this.IsAuthenticated(filterContext))
+						filterContext.Result = new HttpUnauthorizedResult();
+
+					return;
 			}
 
-			if(controllerAttribute == null && actionAttribute == null)
-				return;
-
-			var principal = filterContext.HttpContext.User as Zongsoft.Security.CertificationPrincipal;
-
-			if(principal == null || principal.Identity == null || (!principal.Identity.IsAuthenticated))
-				this.Failed(filterContext, "没有通过身份验证，拒绝访问。");
-
-			var authorization = _authorization;
+			//获取授权验证服务
+			var authorization = this.Authorization;
 
 			if(authorization == null)
-				this.Failed(filterContext, "没有可用的授权验证程序，拒绝访问。");
+				throw new MissingMemberException(this.GetType().FullName, "Authorization");
 
-			if(!authorization.IsAuthorized(principal.Identity.Certification.User.UserId, schemaId, actionId))
-				this.Failed(filterContext, "权限不够，访问被拒绝。");
+			//执行授权验证操作，如果验证失败则返回验证失败的响应
+			if(!authorization.IsAuthorized(((CertificationPrincipal)filterContext.HttpContext.User).Identity.Certification.User.UserId, schemaId, actionId))
+				filterContext.Result = new HttpStatusCodeResult(System.Net.HttpStatusCode.Forbidden);
+		}
+		#endregion
+
+		#region 虚拟方法
+		protected virtual bool IsAuthenticated(AuthorizationContext filterContext)
+		{
+			var principal = filterContext.HttpContext.User as Zongsoft.Security.CertificationPrincipal;
+
+			if(principal == null || principal.Identity == null)
+				return false;
+
+			return principal.Identity.IsAuthenticated;
 		}
 		#endregion
 
 		#region 私有方法
-		private string GetSchemaId(string text)
+		private AuthorizationMode GetAuthorizationMode(AuthorizationContext filterContext, out string schemaId, out string actionId)
 		{
-			if(text != null && text.Length > 10 && text.EndsWith("Controller", StringComparison.OrdinalIgnoreCase))
-				return text.Substring(0, text.Length - 10);
+			schemaId = null;
+			actionId = null;
 
-			return text;
+			//查找位于Action方法的授权标记
+			var attribute = (AuthorizationAttribute)filterContext.ActionDescriptor.GetCustomAttributes(typeof(Zongsoft.Security.Membership.AuthorizationAttribute), true).FirstOrDefault();
+
+			if(attribute == null)
+			{
+				//查找位于Controller类的授权标记
+				attribute = (AuthorizationAttribute)Attribute.GetCustomAttribute(filterContext.Controller.GetType(), typeof(Zongsoft.Security.Membership.AuthorizationAttribute), true);
+
+				if(attribute == null)
+					return AuthorizationMode.Ignore;
+
+				schemaId = string.IsNullOrWhiteSpace(attribute.SchemaId) ? this.GetSchemaId(filterContext.Controller.GetType().Name, filterContext.RouteData.Values["area"] as string) : attribute.SchemaId;
+				actionId = filterContext.ActionDescriptor.ActionName;
+
+				return attribute.Mode;
+			}
+
+			schemaId = attribute.SchemaId;
+			actionId = string.IsNullOrWhiteSpace(attribute.ActionId) ? filterContext.ActionDescriptor.ActionName : attribute.ActionId;
+
+			if(string.IsNullOrWhiteSpace(schemaId))
+			{
+				var controllerAttribute = (AuthorizationAttribute)Attribute.GetCustomAttribute(filterContext.Controller.GetType(), typeof(Zongsoft.Security.Membership.AuthorizationAttribute), true);
+
+				if(controllerAttribute == null || string.IsNullOrWhiteSpace(controllerAttribute.SchemaId))
+					schemaId = this.GetSchemaId(filterContext.Controller.GetType().Name, filterContext.RouteData.Values["area"] as string);
+				else
+					schemaId = controllerAttribute.SchemaId;
+			}
+
+			return attribute.Mode;
 		}
 
-		private void Failed(AuthorizationContext filterContext, string message)
+		private string GetSchemaId(string name, string areaName)
 		{
-			filterContext.Result = new HttpUnauthorizedResult(message);
+			if(name != null && name.Length > 10 && name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase))
+				name = name.Substring(0, name.Length - 10);
+
+			if(string.IsNullOrWhiteSpace(areaName))
+				return name;
+
+			return areaName.Replace('/', '-') + "-" + name;
 		}
 		#endregion
 	}
