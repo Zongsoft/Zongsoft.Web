e7582b88a95379dece0a7cac43333891d932d045
Popeye Zhong <zongsoft@gmail.com>
2016-03-18 20:14:32 +0800

    :+1: :fire: 增加更多的扩展点。 :tulip:

--- src/ControllerBase.cs
+++ src/ControllerBase.cs
@@ -103,14 +103,14 @@ namespace Zongsoft.Web
 		[HttpGet]
 		public virtual ActionResult Index(string id = null, Paging paging = null)
 		{
-			if(string.IsNullOrWhiteSpace(id))
-				return this.View(this.DataService.Select(null, paging));
-
 			//将分页信息传递给视图
 			this.ViewData["Paging"] = paging;
 
+			if(string.IsNullOrWhiteSpace(id))
+				return this.View(this.OnIndex((TConditional)null, null, paging));
+
 			//获取当前请求对应的模型数据
-			var model = this.GetModel(id, paging);
+			var model = this.OnIndex(id, null, paging);
 
 			//如果模型数据为集合类型，则返回“Index”默认视图；否则返回“Details”详细视图
 			if(model != null && (model is System.Collections.IEnumerable || Zongsoft.Common.TypeExtension.IsAssignableFrom(typeof(IEnumerable<>), model.GetType())))
@@ -122,15 +122,12 @@ namespace Zongsoft.Web
 		[HttpPost]
 		public virtual ActionResult Index(TConditional conditional, Paging paging = null)
 		{
-			if(conditional == null)
-				return this.Index(string.Empty, paging);
-
 			//将分页信息传递给视图
 			this.ViewData["Paging"] = paging;
 			//将查询条件传递给视图
 			this.ViewData["Conditional"] = conditional;
 
-			return this.View(this.DataService.Select(conditional, paging));
+			return this.View(this.OnIndex(conditional, null, paging));
 		}
 
 		[HttpGet]
@@ -139,28 +136,25 @@ namespace Zongsoft.Web
 			if(string.IsNullOrWhiteSpace(id))
 				return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
 
-			return this.View(this.GetModel(id));
+			return this.View(this.OnIndex(id));
 		}
 
-		[HttpPut, HttpPost]
-		public virtual ActionResult Edit(string id, TModel model, string redirectUrl = null)
+		[HttpPost]
+		public virtual ActionResult Edit(TModel model, string redirectUrl = null)
 		{
-			if(string.IsNullOrWhiteSpace(id))
-				return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-
 			if(model == null)
 				return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
 
 			try
 			{
-				if(this.DataService.Update(model) > 0)
+				if(this.OnEdit(model) > 0)
 				{
 					if(!string.IsNullOrWhiteSpace(redirectUrl))
 						return this.Redirect(redirectUrl);
 				}
 				else
 				{
-					this.ModelState.AddModelError(string.Empty, Zongsoft.Resources.ResourceUtility.GetString("Text.DataUpdateFailed.Message"));
+					this.ModelState.AddModelError(string.Empty, Resources.ResourceUtility.GetString("Text.DataUpdateFailed"));
 				}
 			}
 			catch(Exception ex)
@@ -174,7 +168,7 @@ namespace Zongsoft.Web
 		[HttpGet]
 		public virtual ActionResult Create()
 		{
-			return this.View(Activator.CreateInstance<TModel>());
+			return this.View();
 		}
 
 		[HttpPost]
@@ -185,14 +179,14 @@ namespace Zongsoft.Web
 
 			try
 			{
-				if(this.DataService.Insert(model) > 0)
+				if(this.OnCreate(model) > 0)
 				{
 					if(!string.IsNullOrWhiteSpace(redirectUrl))
 						return this.Redirect(redirectUrl);
 				}
 				else
 				{
-					this.ModelState.AddModelError(string.Empty, Zongsoft.Resources.ResourceUtility.GetString("Text.DataCreateFailed.Message"));
+					this.ModelState.AddModelError(string.Empty, Resources.ResourceUtility.GetString("Text.DataCreateFailed"));
 				}
 			}
 			catch(Exception ex)
@@ -206,13 +200,31 @@ namespace Zongsoft.Web
 		[HttpGet]
 		public virtual ActionResult Delete(string id)
 		{
-			var model = this.DataService.Get<string>(id);
+			var model = this.OnIndex(id);
 			return this.View(model);
+		}
+
+		[HttpDelete]
+		public virtual ActionResult Delete(string id, string redirectUrl = null)
+		{
+			if(string.IsNullOrWhiteSpace(id))
+				return this.View();
+
+			if(this.OnDelete(id) < 1)
+			{
+				this.ModelState.AddModelError(string.Empty, Resources.ResourceUtility.GetString("Text.DataDeleteFailed"));
+				return this.View();
+			}
+
+			if(string.IsNullOrWhiteSpace(redirectUrl))
+				return this.View("Index");
+			else
+				return this.Redirect(redirectUrl);
 		}
 		#endregion
 
-		#region 私有方法
-		private object GetModel(string id, Paging paging = null)
+		#region 保护方法
+		protected virtual object OnIndex(string id, string scope = null, Paging paging = null, params Sorting[] sortings)
 		{
 			if(string.IsNullOrWhiteSpace(id))
 				return null;
@@ -222,14 +234,49 @@ namespace Zongsoft.Web
 			switch(parts.Length)
 			{
 				case 1:
-					return this.DataService.Get<string>(parts[0], paging);
+					return this.DataService.Get<string>(parts[0], scope, paging, sortings);
 				case 2:
-					return this.DataService.Get<string, string>(parts[0], parts[1], paging);
+					return this.DataService.Get<string, string>(parts[0], parts[1], scope, paging, sortings);
 				case 3:
-					return this.DataService.Get<string, string, string>(parts[0], parts[1], parts[2], paging);
+					return this.DataService.Get<string, string, string>(parts[0], parts[1], parts[2], scope, paging, sortings);
 				default:
-					return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+					return null;
 			}
+		}
+
+		protected virtual object OnIndex(TConditional conditional, string scope = null, Paging paging = null, params Sorting[] sortings)
+		{
+			return this.DataService.Select(conditional, scope, paging, sortings);
+		}
+
+		protected virtual int OnDelete(string id, string cascades = null)
+		{
+			if(string.IsNullOrWhiteSpace(id))
+				return 0;
+
+			var parts = id.Split('-');
+
+			switch(parts.Length)
+			{
+				case 1:
+					return this.DataService.Delete<string>(parts[0], cascades);
+				case 2:
+					return this.DataService.Delete<string, string>(parts[0], parts[1], cascades);
+				case 3:
+					return this.DataService.Delete<string, string, string>(parts[0], parts[1], parts[2], cascades);
+			}
+
+			return -1;
+		}
+
+		protected virtual int OnEdit(TModel model, string scope = null, ICondition condition = null)
+		{
+			return this.DataService.Update(model, condition, scope);
+		}
+
+		protected virtual int OnCreate(TModel model, string scope = null)
+		{
+			return this.DataService.Insert(model, scope);
 		}
 		#endregion
 	}
