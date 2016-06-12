b2eaba3cbe14501dae06dcc8e431a9f8433dee70
Popeye Zhong <zongsoft@gmail.com>
2016-06-12 17:14:25 +0800

    feat: 添加HTTP版本的身份验证过滤器。 :shaved_ice:

---
+++ src/Http/Security/AuthenticationFilter.cs
@@ -0,0 +1,115 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2016 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web.
+ *
+ * Zongsoft.Web is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Web; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+using System.Web.Http.Filters;
+
+using Zongsoft.Security;
+using Zongsoft.Security.Membership;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zongsoft.Web.Http.Security
+{
+	public class AuthenticationFilter : IAuthenticationFilter
+	{
+		#region 成员字段
+		private ICredentialProvider _credentialProvider;
+
+		public bool AllowMultiple
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+		#endregion
+
+		#region 公共属性
+		[Zongsoft.Services.ServiceDependency]
+		public ICredentialProvider CredentialProvider
+		{
+			get
+			{
+				return _credentialProvider;
+			}
+			set
+			{
+				if(value == null)
+					throw new ArgumentNullException();
+
+				_credentialProvider = value;
+			}
+		}
+		#endregion
+
+		#region 验证实现
+		public async Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
+		{
+			if(context.Request.Headers.Authorization == null || !string.Equals(context.Request.Headers.Authorization.Scheme, "API", StringComparison.OrdinalIgnoreCase))
+				return;
+
+			var credentialId = GetAuthenticationKey(context.Request.Headers.Authorization.Parameter);
+
+			if(string.IsNullOrWhiteSpace(credentialId))
+				context.Principal = CredentialPrincipal.Empty;
+			else
+				context.Principal = new CredentialPrincipal(new CredentialIdentity(credentialId, this.CredentialProvider));
+		}
+
+		public async Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
+		{
+			var principal = context.ActionContext.RequestContext.Principal;
+
+			if(AuthenticationUtility.IsAuthenticated(principal) || AuthenticationUtility.GetAuthorizationMode(context.ActionContext.ActionDescriptor) == AuthorizationMode.Disabled)
+				return;
+
+			var challenge = new System.Net.Http.Headers.AuthenticationHeaderValue("API");
+			context.Result = new System.Web.Http.Results.UnauthorizedResult(new[] { challenge }, context.Request);
+		}
+		#endregion
+
+		#region 私有方法
+		private static string GetAuthenticationKey(string text)
+		{
+			if(string.IsNullOrWhiteSpace(text))
+				return null;
+
+			int index = text.IndexOf(':');
+
+			if(index > 0)
+				return text.Substring(0, index);
+
+			return null;
+		}
+		#endregion
+	}
+}
---
+++ src/Http/Security/AuthenticationUtility.cs
@@ -0,0 +1,127 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2016 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web.
+ *
+ * Zongsoft.Web is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Web; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Security.Cryptography;
+
+using Zongsoft.Security;
+using Zongsoft.Security.Membership;
+
+namespace Zongsoft.Web.Http.Security
+{
+	internal static class AuthenticationUtility
+	{
+		public static bool IsAuthenticated(System.Security.Principal.IPrincipal principal)
+		{
+			return (principal != null && principal.Identity != null && principal.Identity.IsAuthenticated);
+		}
+
+		internal static AuthorizationAttribute GetAuthorizationAttribute(HttpActionDescriptor actionDescriptor)
+		{
+			//查找位于Action方法的授权标记
+			var attribute = actionDescriptor.GetCustomAttributes<AuthorizationAttribute>(true).FirstOrDefault();
+
+			//查找位于Controller类的授权标记
+			if(attribute == null)
+				attribute = actionDescriptor.ControllerDescriptor.GetCustomAttributes<AuthorizationAttribute>(true).FirstOrDefault();
+
+			return attribute;
+		}
+
+		internal static AuthorizationMode GetAuthorizationMode(HttpActionDescriptor actionDescriptor)
+		{
+			var attribute = GetAuthorizationAttribute(actionDescriptor);
+
+			if(attribute == null)
+				return AuthorizationMode.Disabled;
+
+			return attribute.Mode;
+		}
+
+		internal static AuthorizationAttribute GetAuthorizationAttribute(HttpActionDescriptor actionDescriptor, System.Web.Routing.RequestContext requestContext)
+		{
+			//查找位于Action方法的授权标记
+			var attribute = actionDescriptor.GetCustomAttributes<AuthorizationAttribute>(true).FirstOrDefault();
+
+			if(attribute == null)
+			{
+				//查找位于Controller类的授权标记
+				attribute = actionDescriptor.ControllerDescriptor.GetCustomAttributes<AuthorizationAttribute>(true).FirstOrDefault();
+
+				if(attribute == null)
+					return null;
+
+				if(attribute.Mode == AuthorizationMode.Required)
+				{
+					if(string.IsNullOrWhiteSpace(attribute.SchemaId))
+						return new AuthorizationAttribute(GetSchemaId(actionDescriptor.ControllerDescriptor.ControllerName, requestContext.RouteData.Values["area"] as string)) { ValidatorType = attribute.ValidatorType };
+				}
+
+				return attribute;
+			}
+
+			if(attribute.Mode == AuthorizationMode.Required)
+			{
+				string schemaId = attribute.SchemaId, actionId = attribute.ActionId;
+
+				if(string.IsNullOrWhiteSpace(schemaId))
+				{
+					var controllerAttribute = actionDescriptor.ControllerDescriptor.GetCustomAttributes<AuthorizationAttribute>(true).FirstOrDefault();
+
+					if(controllerAttribute == null || string.IsNullOrWhiteSpace(controllerAttribute.SchemaId))
+						schemaId = GetSchemaId(actionDescriptor.ControllerDescriptor.ControllerName, requestContext.RouteData.Values["area"] as string);
+					else
+						schemaId = controllerAttribute.SchemaId;
+				}
+
+				if(string.IsNullOrWhiteSpace(actionId))
+					actionId = actionDescriptor.ActionName;
+
+				return new AuthorizationAttribute(schemaId, actionId) { ValidatorType = attribute.ValidatorType };
+			}
+
+			return attribute;
+		}
+
+		private static string GetSchemaId(string name, string areaName)
+		{
+			if(name != null && name.Length > 10 && name.EndsWith("Controller", StringComparison.OrdinalIgnoreCase))
+				name = name.Substring(0, name.Length - 10);
+
+			if(string.IsNullOrWhiteSpace(areaName))
+				return name;
+
+			return areaName.Replace('/', '-') + "-" + name;
+		}
+
+	}
+}
