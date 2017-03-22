231e7baab5ab973befc21b10d656def3f8b8f6c4
Popeye Zhong <9555843@qq.com>
2017-03-23 00:14:31 +0800

    feat: 移除输入验证过滤器。 :inbox_tray:

---
+++ src/Http/Security/AuthorizationFilter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Controllers;
+
+namespace Zongsoft.Web.Http.Security
+{
+	public class AuthorizationFilter : System.Web.Http.Filters.IAuthorizationFilter
+	{
+		public bool AllowMultiple
+		{
+			get
+			{
+				return true;
+			}
+		}
+
+		public Task<HttpResponseMessage> ExecuteAuthorizationFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+		{
+			return continuation();
+		}
+	}
+}
