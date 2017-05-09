77c472b14b896dbb9f7dd8c7a1b409a7d51f03c8
Popeye Zhong <9555843@qq.com>
2017-05-09 16:40:11 +0800

    feat: 增加了自定义的JsonFormatter内容格式化器。 :racehorse: :whale:

---
+++ src/Http/JsonMediaTypeFormatter.cs
@@ -0,0 +1,143 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2017 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web.
+ *
+ * Zongsoft.Web is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Web; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Formatting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zongsoft.Web.Http
+{
+	public class JsonMediaTypeFormatter : System.Net.Http.Formatting.MediaTypeFormatter
+	{
+		#region 静态常量
+		private static readonly System.Net.Http.Headers.MediaTypeHeaderValue ApplicationJsonMediaType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+		private static readonly System.Net.Http.Headers.MediaTypeHeaderValue TextJsonMediaType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/json");
+		#endregion
+
+		#region 构造函数
+		public JsonMediaTypeFormatter()
+		{
+			this.SupportedEncodings.Add(new System.Text.UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true));
+			this.SupportedEncodings.Add(new System.Text.UnicodeEncoding(bigEndian: false, byteOrderMark: true, throwOnInvalidBytes: true));
+
+			this.SupportedMediaTypes.Add(ApplicationJsonMediaType);
+			this.SupportedMediaTypes.Add(TextJsonMediaType);
+
+			this.MediaTypeMappings.Add(new XmlHttpRequestHeaderMapping());
+		}
+		#endregion
+
+		#region 重写方法
+		public override bool CanReadType(Type type)
+		{
+			if(type == null)
+				throw new ArgumentNullException(nameof(type));
+
+			return true;
+		}
+
+		public override bool CanWriteType(Type type)
+		{
+			if(type == null)
+				throw new ArgumentNullException(nameof(type));
+
+			return true;
+		}
+
+		public override Task<object> ReadFromStreamAsync(Type type, Stream readStream, HttpContent content, IFormatterLogger formatterLogger)
+		{
+			if(type == null)
+				throw new ArgumentNullException(nameof(type));
+			if(readStream == null)
+				throw new ArgumentNullException(nameof(readStream));
+
+			try
+			{
+				return Task.FromResult(this.ReadFromStream(type, readStream, content, formatterLogger));
+			}
+			catch(Exception e)
+			{
+				return TaskHelper.FromError<object>(e);
+			}
+		}
+
+		private object ReadFromStream(Type type, Stream readStream, HttpContent content, IFormatterLogger formatterLogger)
+		{
+			HttpContentHeaders contentHeaders = content == null ? null : content.Headers;
+
+			if(contentHeaders != null && contentHeaders.ContentLength == 0)
+				return GetDefaultValueForType(type);
+
+			var effectiveEncoding = this.SelectCharacterEncoding(contentHeaders);
+
+			try
+			{
+				return Zongsoft.Runtime.Serialization.Serializer.Json.Deserialize(readStream, type);
+			}
+			catch(Exception e)
+			{
+				if(formatterLogger == null)
+					throw;
+
+				formatterLogger.LogError(String.Empty, e);
+				return GetDefaultValueForType(type);
+			}
+		}
+
+		public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content, TransportContext transportContext, CancellationToken cancellationToken)
+		{
+			if(type == null)
+				throw new ArgumentNullException(nameof(type));
+			if(writeStream == null)
+				throw new ArgumentNullException(nameof(writeStream));
+
+			if(cancellationToken.IsCancellationRequested)
+				return TaskHelper.Canceled();
+
+			try
+			{
+				this.WriteToStream(type, value, writeStream, content);
+				return TaskHelper.Completed();
+			}
+			catch(Exception ex)
+			{
+				return TaskHelper.FromError(ex);
+			}
+		}
+
+		private void WriteToStream(Type type, object value, Stream writeStream, HttpContent content)
+		{
+			var effectiveEncoding = this.SelectCharacterEncoding(content == null ? null : content.Headers);
+			Zongsoft.Runtime.Serialization.Serializer.Json.Serialize(writeStream, value);
+		}
+		#endregion
+	}
+}
---
+++ src/TaskHelper.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Authors:
+ *   钟峰(Popeye Zhong) <zongsoft@gmail.com>
+ *
+ * Copyright (C) 2017 Zongsoft Corporation <http://www.zongsoft.com>
+ *
+ * This file is part of Zongsoft.Web.
+ *
+ * Zongsoft.Web is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * Zongsoft.Web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Zongsoft.Web; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+using System;
+using System.Threading.Tasks;
+
+namespace Zongsoft.Web
+{
+	internal static class TaskHelper
+	{
+		private struct AsyncVoid
+		{
+		}
+
+		private static readonly Task _defaultCompleted = Task.FromResult<AsyncVoid>(default(AsyncVoid));
+		private static readonly Task<object> _completedTaskReturningNull = Task.FromResult<object>(null);
+
+		/// <summary>
+		/// 返回一个被取消的任务。
+		/// </summary>
+		internal static Task Canceled()
+		{
+			return CancelCache<AsyncVoid>.Canceled;
+		}
+
+		/// <summary>
+		/// 返回一个被取消的特定类型的任务。
+		/// </summary>
+		internal static Task<TResult> Canceled<TResult>()
+		{
+			return CancelCache<TResult>.Canceled;
+		}
+
+		/// <summary>
+		/// 返回一个没有结果的已完成的任务。
+		/// </summary>        
+		internal static Task Completed()
+		{
+			return _defaultCompleted;
+		}
+
+		/// <summary>
+		/// 返回一个出错的任务。
+		/// </summary>
+		internal static Task FromError(Exception exception)
+		{
+			return FromError<AsyncVoid>(exception);
+		}
+
+		/// <summary>
+		/// 返回一个出错的特定类型的任务
+		/// </summary>
+		internal static Task<TResult> FromError<TResult>(Exception exception)
+		{
+			TaskCompletionSource<TResult> tcs = new TaskCompletionSource<TResult>();
+			tcs.SetException(exception);
+			return tcs.Task;
+		}
+
+		internal static Task<object> NullResult()
+		{
+			return _completedTaskReturningNull;
+		}
+
+		private static class CancelCache<TResult>
+		{
+			public static readonly Task<TResult> Canceled = GetCancelledTask();
+
+			private static Task<TResult> GetCancelledTask()
+			{
+				TaskCompletionSource<TResult> tcs = new TaskCompletionSource<TResult>();
+				tcs.SetCanceled();
+				return tcs.Task;
+			}
+		}
+	}
+}
